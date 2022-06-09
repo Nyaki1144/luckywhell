3f6749c2e17ba444499c0f3c29f7d98a31c18297
Nyaki1144 <narek1144@gmail.com>
2022-06-09 15:12:08 +0400

    add: create pop-up

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const test = document.querySelector('.main-img')
+    const test = document.querySelector('.main-img');
+    const close = document.querySelector('.close');
+    const popup = document.querySelector('.popup');
+    const moreInfo = document.querySelector('.more-info')
+
+
+    popup.addEventListener('click', (e) => {
+      console.log(e.target)
+      if(e.target.classList.contains("popup") ||
+         e.target.classList.contains("icon") ||
+         e.target.classList.contains("iconX")
+      ){
+              popup.style.display = 'none'
+      }
+    })
+
+    moreInfo.addEventListener('click', (e) => {
+      console.log(e.currentTarget)
+      if(e.currentTarget.classList.contains("more-info") ){
+              popup.style.display = 'block'
+      }
+    })
 
 
   anime(
